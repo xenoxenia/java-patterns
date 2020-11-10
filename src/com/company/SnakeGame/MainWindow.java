941d4ec2fb64f9dc4cc11b36c0d19f17baef7ace
Xeno_xenia <deadbyjul@gmail.com>
2020-11-10 13:43:53 +0300

    17 практическая, змейка, но фиолетовая

---
+++ src/com/company/SnakeGame/MainWindow.java
@@ -0,0 +1,18 @@
+package com.company.SnakeGame;
+
+import javax.swing.*;
+
+public class MainWindow extends JFrame {
+    public MainWindow(){
+        setTitle("Snake");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(500, 500);
+        setLocation(400, 400);
+        add(new GameField());
+        setVisible(true);
+    }
+
+    public static void main(String[] args){
+        MainWindow mw = new MainWindow();
+    }
+}
